8ad145da02c9c5a18cf28853182108afa77b3099
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2024-03-16 17:36:35 -0300

    configuracion inicial terminada

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import morgan from "morgan";
+import path from 'path'
+import { fileURLToPath } from "url";
+
+const app = express();
+app.set("port", process.env.PORT || 4000);
+
+app.listen(app.get("port"), () => {
+  console.info("Estoy en el puerto " + app.get("port"));
+});
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const __filaname = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filaname);
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+
