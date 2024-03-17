151eb5f8b951380013a7210000b36bc22c29a762
Sebastian Agustin Maza <sebas030.maza@gmail.com>
2024-03-17 15:46:30 -0300

    se agrego la funcion para borrar una tarea a la ruta para hacer la solicitud delete

--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import {
+  borrarTarea,
   crearTarea,
   editarTarea,
   listarTareas,
@@ -9,6 +10,6 @@ import {
 const router = Router();
 
 router.route("/tareas").post(crearTarea).get(listarTareas);
-router.route("/tareas/:id").get(obtenerTarea).put(editarTarea);
+router.route("/tareas/:id").get(obtenerTarea).put(editarTarea).delete(borrarTarea);
 
 export default router;
